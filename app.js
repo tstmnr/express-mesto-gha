d830b0ea8b31958d4a848d22899d6ecf489eeea0
Eugeniy Razveev <razveev.evg@gmail.com>
2023-04-19 22:12:27 +0300

    connect mongodb

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+// подключаем мидлвары, роуты и всё остальное...
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
